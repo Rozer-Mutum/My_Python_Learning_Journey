abef21c1de41901308b4e160c654fcdcd5e8ab32
Rozer-Mutum <Rozer.Mutum@yokogawa.com>
2022-05-30 09:26:59 +0530

    For day one

---
+++ first_file.py
@@ -0,0 +1,10 @@
+print("DAY 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what want to print')")
+
+#passing \n to print in new line
+print("This is the first line \nThis is the second line")
+
+#adding two strings using + sign
+print("Hello" + " " + "Tom")
+print("Hello" + " Tom")
